591646d75a32808bd0123fce2d25798cee7165e5
Minh Hoang <minh.le@fruitful.io>
2018-02-14 16:36:41 +0700

    Update Full HTML

--- js/script.js
+++ js/script.js
@@ -13,4 +13,108 @@ $(document).ready(function () {
     scrollContainer: null // optional scroll container selector, otherwise use window
   });
   wow.init();
+
+  function setCopyrightDate() {
+    var year = new Date().getYear();
+    if (year < 1900)
+      year += 1900;
+    document.getElementById("currentYear").innerHTML = year;
+  }
+  setCopyrightDate();
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    autoplay: true,
+    autoplaySpeed: 3500
+  });
+
+  $('.slider-nav').slick({
+    autoplay: true,
+    autoplaySpeed: 3500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
+  });
+
+
+  $('.product-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.screen-view').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 0,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $('.product-view').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    arrows: true,
+    infinite: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 1070,
+        settings: {
+          centerPadding: '100px',
+          arrows: true,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: '20px',
+          arrows: true,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.feedback-view').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    autoplaySpeed: 2000,
+    infinite: true
+  });
 });
